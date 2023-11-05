e75a906bd77dbc8a9e4332b33c6f75eb0eadaed9
Lauren <lauren.m.lavelle@wmich.edu>
2023-11-05 15:23:26 -0500

    add bycrypt for passwords

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,7 @@
 // Validation and bycrypt password
 
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -46,6 +47,19 @@ const userSchema = new Schema(
     },
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
