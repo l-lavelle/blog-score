82c969db50885129cdb9baac0df385e46deeb3cc
mHammy <133612424+mHammy@users.noreply.github.com>
2023-11-08 15:50:24 -0500

    Additional changes, webpage is now viewable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import MainContent from './components/MainContent';
-// import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import './App.css'; 
 
@@ -15,9 +14,6 @@ function App() {
           <div className="col-md-8">
             <MainContent />
             <h1>Testing</h1>
-          </div>
-          <div className="col-md-4">
-            <Sidebar />
           </div>
         </div>
       </div>
