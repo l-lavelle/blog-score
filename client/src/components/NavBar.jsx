f7294e51db6dc64a7fada941b3e04d17a63345df
Alex Tester <lex.tester93@gmail.com>
2023-11-09 14:58:48 -0500

    Added size change for BlogScore.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,11 +5,11 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className='px-3'>
-      <Navbar.Brand href="#home" className='font-c color-c'>BlogScore</Navbar.Brand>
+      <Navbar.Brand href="#home" className='font-c color-c size-change'>BlogScore</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <div className="flex-grow-1"></div>
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="justify-content-end" style={{ width: "100%" }}>
+        <Nav className="justify-content-end m-3" style={{ width: "100%" }}>
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#recent">Recent</Nav.Link>
           <Nav.Link href="#favorites">Favorites</Nav.Link>
