6e5c3c46283be3c76810a06bbce2ecce5b4570f1
mHammy <133612424+mHammy@users.noreply.github.com>
2023-11-09 18:58:01 -0500

    Login page addition

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,59 @@
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+
 const Login = () => {
-  
-    return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
-    );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
+    await fetch('PENDING BACKEND', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   };
-  
-  export default Login;
+
+  return (
+    <Container className="mt-5">
+      <h2>Login</h2>
+      <Form onSubmit={handleLogin}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Login;
