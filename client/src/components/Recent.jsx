4b466f7eda597d1d09fce60664f3141909a0bd8a
Lauren <lauren.m.lavelle@wmich.edu>
2023-11-12 22:05:41 -0500

    fixing errors

--- client/src/components/Recent.jsx
+++ client/src/components/Recent.jsx
@@ -4,16 +4,16 @@ import { RECENT_POSTS_QUERY } from '../utils/queries';
 
 const Recent = () => {
   const { loading, error, data } = useQuery(RECENT_POSTS_QUERY);
-
+console.log(data)
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error</p>;
 
   return (
     <div>
       <h1>Recent Posts</h1>
       {data.recentPosts.map((post) => (
         <div key={post._id}>
-          <h3>{post.title}</h3>
+          <h3>{post.postTitle}</h3>
         </div>
       ))}
     </div>
