3e892d334a67eadd15e6e5230fe485f756a405ab
Lauren <lauren.m.lavelle@wmich.edu>
2023-12-05 17:49:44 -0500

    added error messages to contact page

--- client/src/components/Footer/Contact.jsx
+++ client/src/components/Footer/Contact.jsx
@@ -1,18 +1,37 @@
 // Need to fix layout but does work!! messages and clear message info
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import  { useState } from 'react';
+// import dotenv from 'dotenv'
+// dotenv.config()
 
 const Contact=()=>{
+    const [message, setMesage]=useState({message:'', status:''});
+    const [emailData, setEmailData] = useState({ message: '', user_email: '', user_name:''});
+    // console.log("trial env",import.meta.env.VITE_SOME_KEY);
+    // console.log("React env",import.meta.env.VITE_SOME_KEY);
+
+    const updateData= async (event)=>{
+        const { name, value } = event.target;
+        setEmailData({ ...emailData, [name]: value });
+      };
+
     const form = useRef();
 
          const sendEmail = (e) => {
+            
            e.preventDefault(); // prevents the page from reloading when you hit “Send”
         
            emailjs.sendForm("service_il6b8zg", 'template_uigoji9', form.current, 'zhCenTCZqdBMtVbwy')
              .then((result) => {
-                 // show the user a success message
+                setMesage({message:'Email was sent', status:'success'})
+                setEmailData({
+                    message: '',
+                    user_email: '',
+                    user_name:''
+                  })
              }, (error) => {
-                 // show the user an error
+                setMesage({message:'Unable to send email', status:'error'})
              });
          };
 
@@ -21,20 +40,22 @@ const Contact=()=>{
         <>
         <div className="footer-info">
             <h1>Contact Us</h1>
-            <p>We always love to here from you </p>
+            <p>We always love to here from our users! Feel free to contact us using the form below and we will get back to you as soon as possible.</p>
         </div>
          
         
         <div >
            <form ref={form} onSubmit={sendEmail} className='d-flex flex-column'>
              <label style={{color:"white"}}>Name</label>
-             <input type="text" name="user_name" />
+             <input onChange={updateData} value={emailData.user_name} type="text" name="user_name" />
              <label style={{color:"white"}}>Email</label>
-             <input type="email" name="user_email" />
+             <input onChange={updateData} value={emailData.user_email} type="email" name="user_email" />
              <label style={{color:"white"}}>Message</label>
-             <textarea name="message" />
+             <textarea onChange={updateData} value={emailData.message} rows={12} name="message" />
              <input type="submit" value="Send" />
            </form>
+           {message.status==='error'?<p className='text-center mt-3' style={{color:"red"}}>{message.message}</p>:null}
+           {message.status==='success'?<p className='text-center mt-3' style={{color:"green"}}>{message.message}</p>:null}
         </div>
          
         </>
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,30 +1,8 @@
-import { useEffect, useRef } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Footer.css'; 
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  const stripeButtonRef = useRef(null);
-
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://js.stripe.com/v3/buy-button.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    const stripeButton = document.createElement('stripe-buy-button');
-    stripeButton.setAttribute('buy-button-id', 'buy_btn_1OCCB2A2p3lCuJJsfZaJfShf');
-    stripeButton.setAttribute('publishable-key', 'pk_live_51OC6JCA2p3lCuJJsAH88i2FWequKFvKIPMntoVtY1AZuOgYWXhO1xjOrKZNDWnGYavpZ5PiZx4x3AE0KgeU2Lo4i00j0ZUYIwX');
-
-    if (stripeButtonRef.current) {
-      stripeButtonRef.current.appendChild(stripeButton);
-    }
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <Container fluid className="footer-container fixed-bottom footer-rounded">
       <Row>
